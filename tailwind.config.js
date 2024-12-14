cd5915d3b2b41517afd6e36b421be555d3a67466
hdavidpb <68169674+hdavidpb@users.noreply.github.com>
2024-12-13 21:04:10 -0500

    ⚒️FIX: Daisy import

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
+
+import daisyui from 'daisyui'
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx,vue}'],
   theme: {
     extend: {},
   },
-  // eslint-disable-next-line @typescript-eslint/no-require-imports
-  plugins: [require('daisyui')],
+
+  plugins: [daisyui],
 }
